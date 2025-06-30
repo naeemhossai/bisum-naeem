342ae130c4f8d754cca08c4101f9e3b3ecb62a75
DevSpreeThemes <devspreethemes@gmail.com>
2025-07-01 03:07:08 +0600

    category done

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -289,3 +289,31 @@ class newProductSlider extends HTMLElement {
 }
 
 customElements.define("product-slider", newProductSlider);
+
+// mobile submenu
+class MobileSubmenu extends HTMLElement {
+  constructor(){
+    super();
+
+    this.opener = this.querySelectorAll('.open-submenu');
+    this.btnBack = this.querySelectorAll('.btn-menu-back');
+
+    this.opener.forEach(item => {
+      item.addEventListener('click', this.open);
+    })
+
+    this.btnBack.forEach(item => {
+      item.addEventListener('click', this.close);
+    })
+  }
+
+  open(event) {
+    event.target.parentElement.parentElement.querySelector('.submenu-transform').classList.add('active');
+  }
+
+  close(event) {
+    event.target.closest('.submenu-transform').classList.remove('active');
+  }
+}
+
+customElements.define('mobile-submenu', MobileSubmenu);
